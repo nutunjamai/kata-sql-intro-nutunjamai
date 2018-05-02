7d7ff728438fe26265e9d62f964a00f9c2c09fc6
Skylar Carter <carter.skylar@gmail.com>
2018-05-02 07:54:56 -0500

    i think i got it

---
+++ SqlIntro/DapperProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace SqlIntro
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly string _connectionString;
+
+        public DapperProductRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                return conn.Query<Product>("SELECT ProductID AS id, Name FROM product;");
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                conn.Execute("DELETE FROM product WHERE ProductId = @id", new { id });
+            }
+        }
+        public void InsertProduct(Product prod)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                conn.Execute("INSERT INTO product (name) values(@name)", new { prod.Name });
+            }
+        }
+
+        public void UpdateProduct(Product prod)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                conn.Execute("UPDATE product SET name = @name WHERE id = @id", new { prod.Name });
+            }
+        }
+
+        public IEnumerable<Product> GetProductsWithReview()
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                return conn.Query<Product>("SELECT p.ProductId, p.Name, pr.Comments FROM product AS p INNER JOIN productreview AS pr ON p.ProductId = pr.ProductId;");
+            }
+        }
+
+        public IEnumerable<Product> GetProductsAndReviews()
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                return conn.Query<Product>("SELECT p.ProductId, p.Name, pr.Comments FROM product AS p LEFT OUTER JOIN productreview AS pr ON p.ProductId = pr.ProductId;");
+            }
+        }
+    }
+}
--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -6,18 +6,25 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            var connectionString = ""; //get connectionString format from connectionstrings.com and change to match your database
-            var repo = new ProductRepository(connectionString);
+            var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=password;"; 
+            var repo = new DapperProductRepository(connectionString);
 
             foreach (var prod in repo.GetProducts())
             {
                 Console.WriteLine("Product Name:" + prod.Name);
             }
 
-           
+            foreach (var prod in repo.GetProductsWithReview())
+            {
+                Console.WriteLine("\nProduct Name:" + prod.Name + "\nProduct Review:" + prod.Comments);
+            }
+
+            foreach (var prod in repo.GetProductsAndReviews())
+            {
+                Console.WriteLine("\nProduct Name:" + prod.Name + "\nProduct Review (if available):" + prod.Comments);
+            }
+
             Console.ReadLine();
         }
-
-       
     }
 }
