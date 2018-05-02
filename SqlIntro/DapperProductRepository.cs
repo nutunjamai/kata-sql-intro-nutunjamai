a48a49e9be8ba682106ffd49d20f20cef46e63cf
Skylar Carter <carter.skylar@gmail.com>
2018-05-02 08:36:27 -0500

    Ok now i think i got it working right

--- SqlIntro/DapperProductRepository.cs
+++ SqlIntro/DapperProductRepository.cs
@@ -65,7 +65,8 @@ namespace SqlIntro
             using (var conn = new MySqlConnection(_connectionString))
             {
                 conn.Open();
-                return conn.Query<Product>("SELECT p.ProductId, p.Name, pr.Comments FROM product AS p LEFT OUTER JOIN productreview AS pr ON p.ProductId = pr.ProductId;");
+                return conn.Query<Product>("SELECT p.ProductId, p.Name, pr.Comments FROM product AS p " +
+                                           "LEFT OUTER JOIN productreview AS pr ON p.ProductId = pr.ProductId;");
             }
         }
     }
